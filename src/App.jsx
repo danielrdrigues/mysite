f87d682e3562dc186a900ca6e29b061b1e500cc6
danielrdrigues <daniel.rodrigues1408@gmail.com>
2023-09-08 14:09:36 -0300

    UPDATE font

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import styles from './App.module.css';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
+    <div className={styles.App}>
+      <header className={styles.header}>
        <h1>Daniel Rodrigues</h1>
         <div id="desc">Welcome to my homepage! My goal with this website is to add and test features and resources, experiment with junk-code and explore new tech. This old-school aesthetic is intentional, just because I find it cool. Eventually, new features will be added. You can find the front-end code on my GitHub page.</div>
         <br />
@@ -13,6 +13,7 @@ function App() {
           <ul><a href="https://www.linkedin.com/in/danielrdrigues/" target="blank" rel="mysaite">Linkedin</a></ul>
           <ul><a href="mailto:drdriguesd@gmail.com">Mail</a></ul>
           <ul><a href="https://t.me/Danielrdrigues" target="blank" rel="mysaite">Telegram</a></ul>
+          
         </li>
       </header>
     </div>
